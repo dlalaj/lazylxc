f101e91ed7d7c5b66a06916f18aeab65113a52d2
Denis Lalaj <denis.lalaj@canonical.com>
2025-05-09 14:45:43 -0700

    feat(image): add support for image API
    This commit introduces support for basic interactions with the Images
    API. At this time it is tracking the default LXD project but it will be
    updated to follow tracking the project highlighted in the Project pane.

--- ui/image_view.py
+++ ui/image_view.py
@@ -1,16 +1,73 @@
 from textual.widgets import ListItem, ListView, Label
+from textual import log
 
-labels = ["focal", "jammy", "noble", "plucky"]
+from cmds.image_commands import (
+    list_images,
+    delete_image,
+    ImageRepr,
+)
 
 
 class ImageView(ListView):
+    BINDINGS = [
+        ("d", "delete_image", "Delete image"),
+    ]
+
+    def __init__(
+        self,
+        *children,
+        initial_index=0,
+        name=None,
+        id=None,
+        classes=None,
+        disabled=False,
+    ):
+        super().__init__(
+            *children,
+            initial_index=initial_index,
+            name=name,
+            id=id,
+            classes=classes,
+            disabled=disabled,
+        )
+        self.images = []
+
     def _on_mount(self) -> None:
         return self._refresh()
 
-    def _refresh(self) -> None:
-        # First clear the entire view
+    def _refresh(self):
         self.clear()
+        self.images: list[ImageRepr] = list_images()
 
-        # Dummy load all images under the currently selected project
-        for image in labels:
-            self.append(ListItem(Label(image)))
+        for image in self.images:
+            # An image label is formatted like so: `<RELEASE> -- <SERIAL> -- <FINGERPRINT>`
+            pretty_label = f"{image.release} -- {image.serial} -- {image.fingerprint}"
+            self.append(ListItem(Label(pretty_label)))
+
+    def action_delete_image(self) -> None:
+        """
+        Deletes the selected/highlighted project from the project view or logs a
+        notification if the project cannot be deleted.
+        """
+        selected_image = self.highlighted_child
+        if selected_image is None:
+            return
+
+        try:
+            image_line = selected_image.children[0].renderable
+        except (IndexError, AttributeError) as e:
+            log(f"Error retrieving image name: {e}")
+            return
+
+        # Extract fingerprint from `<RELEASE> -- <SERIAL> -- <FINGERPRINT>`
+        fingerprint = image_line.split()[-1].strip()
+        error = delete_image(fingerprint)
+        if error:
+            self.notify(message=str(error), title="Error deleting image", timeout=2)
+            log(error)
+        else:
+            log("Image deleted successfully")
+        self._refresh()
+
+        # TODO: Complete logic to replace highlighted item
+        # self.action_select_cursor()
